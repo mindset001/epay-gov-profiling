161d7cc2e4c60c6fd14d262274142c4695fe28bc
mindset001 <shittumoshoodademola@gmail.com>
2023-12-03 14:57:54 +0100

    profilee

--- js/index.js
+++ js/index.js
@@ -3,8 +3,15 @@
 function showPage1(){
     document.getElementById('page1').style.display='block'
     document.getElementById('page3').style.display='none'
+    document.getElementById('page2').style.display='none'
+}
+function showPage2(){
+    document.getElementById('page3').style.display='none'
+    document.getElementById('page1').style.display='none'
+    document.getElementById('page2').style.display='block'
 }
 function showPage3(){
     document.getElementById('page3').style.display='block'
     document.getElementById('page1').style.display='none'
+    document.getElementById('page2').style.display='none'
 }
