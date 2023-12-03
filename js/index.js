8cde0c964b82f20e8fbc9f24bac405486a52625f
mindset001 <shittumoshoodademola@gmail.com>
2023-12-03 09:01:38 +0100

    trans

---
+++ js/index.js
@@ -0,0 +1,10 @@
+
+
+function showPage1(){
+    document.getElementById('page1').style.display='block'
+    document.getElementById('page3').style.display='none'
+}
+function showPage3(){
+    document.getElementById('page3').style.display='block'
+    document.getElementById('page1').style.display='none'
+}
